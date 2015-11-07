f59c4de7f5409435ba68b3de84f52eba72bffd0a
Nokius <jupaja@gmx.net>
2015-11-07 22:40:13 +0100

    German Translation

---
+++ translations/harbour-vk-music-de.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <source>Mail to</source>
+        <translation>Mail an</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+</context>
+<context>
+    <name>AlbumsPage</name>
+    <message>
+        <source>Albums</source>
+        <translation>Аlbum</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Aktualisieren</translation>
+    </message>
+</context>
+<context>
+    <name>CacheDirDialog</name>
+    <message>
+        <source>Select path</source>
+        <translation>Wähle Pfad</translation>
+    </message>
+    <message>
+        <source>Local cache</source>
+        <translation>Lokaler Cache</translation>
+    </message>
+    <message>
+        <source>SD card directory</source>
+        <translation>SD Card Ordner</translation>
+    </message>
+    <message>
+        <source>SD card hidden directory</source>
+        <translation>SD Card versteckter Ordner</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Music</source>
+        <translation>Musik</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Aktualisieren</translation>
+    </message>
+</context>
+<context>
+    <name>MinSpaceDialog</name>
+    <message>
+        <source>Select size</source>
+        <translation>Größe wählen</translation>
+    </message>
+    <message>
+        <source>1GB</source>
+        <translation>1GB</translation>
+    </message>
+    <message>
+        <source>2GB</source>
+        <translation>2GB</translation>
+    </message>
+    <message>
+        <source>5GB</source>
+        <translation>5GB</translation>
+    </message>
+    <message>
+        <source>No cache</source>
+        <translation>Kein Cache</translation>
+    </message>
+    <message>
+        <source>No restrictions</source>
+        <translation>Keine Begrenzung</translation>
+    </message>
+</context>
+<context>
+    <name>MusicList</name>
+    <message>
+        <source>My music</source>
+        <translation>Meine Musik</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Suche</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Abmelden</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Anmelden</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Aktualisieren</translation>
+    </message>
+    <message>
+        <source>Error occured</source>
+        <translation>Ein Fehler ist aufgetreten</translation>
+    </message>
+    <message>
+        <source>Show more</source>
+        <translation>Zeige mehr</translation>
+    </message>
+    <message>
+        <source>You will be logged out</source>
+        <translation>Du wirst ausgelogt</translation>
+    </message>
+    <message>
+        <source>Downloading playlist...</source>
+        <translation>Lade Playliste ...</translation>
+    </message>
+    <message>
+        <source>Downloading playlist stopped</source>
+        <translation>Laden der Playliste abbrechen</translation>
+    </message>
+    <message>
+        <source>Shuffle</source>
+        <translation>Shuffle</translation>
+    </message>
+    <message>
+        <source>Please login</source>
+        <translation>Bitte melde dich an</translation>
+    </message>
+</context>
+<context>
+    <name>Notification</name>
+    <message>
+        <source>Error occured</source>
+        <translation>Ein Fehler ist aufgetreten</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Cache directory size</source>
+        <translation>Cache Ordner größe</translation>
+    </message>
+    <message>
+        <source>Clear cache directory</source>
+        <translation>Bereinige Cache Ordner</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Free space available</source>
+        <translation>Verfügbarer Speicher</translation>
+    </message>
+    <message>
+        <source>Minimum free space</source>
+        <translation>Minimal freier Speicher</translation>
+    </message>
+    <message>
+        <source>Cache location</source>
+        <translation>Cache Path</translation>
+    </message>
+    <message>
+        <source>Local cache</source>
+        <translation>Lokaler Cache</translation>
+    </message>
+    <message>
+        <source>SD card directory</source>
+        <translation>SD Card Ordner</translation>
+    </message>
+    <message>
+        <source>SD card hidden directory</source>
+        <translation>SD Card versteckter Ordner</translation>
+    </message>
+    <message>
+        <source>1GB</source>
+        <translation>1GB</translation>
+    </message>
+    <message>
+        <source>2GB</source>
+        <translation>2GB</translation>
+    </message>
+    <message>
+        <source>5GB</source>
+        <translation>5GB</translation>
+    </message>
+    <message>
+        <source>No cache</source>
+        <translation>Kein Cache</translation>
+    </message>
+    <message>
+        <source>No restrictions</source>
+        <translation>Keine Begrenzung</translation>
+    </message>
+    <message>
+        <source>Show bit rate</source>
+        <translation>Zeige die Bitrate</translation>
+    </message>
+</context>
+<context>
+    <name>SongItem</name>
+    <message>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <source>Clear cache</source>
+        <translation>Bereinige Cache Ordner</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Löchen</translation>
+    </message>
+</context>
+</TS>
